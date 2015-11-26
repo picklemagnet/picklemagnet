b022bb6316718fd3afa90160c7aff40dda56b953
Rikke Solbjørg <1645846+solbjorg@users.noreply.github.com>
2015-11-26 20:18:24 +0100

    update to constellations

--- constellations/script.js
+++ constellations/script.js
@@ -1,21 +1,28 @@
-const DRAW_DISTANCE = 70; // distance from cursor to dot before line is drawn
+const DRAW_DISTANCE = 100; // distance from cursor to dot before line is drawn
 
 var mousePos;
+var dots;
 var canvas = {};
 canvas.node = document.getElementById('canvas');
 canvas.context = canvas.node.getContext('2d');
-canvas.node.width  = window.innerWidth;
-canvas.node.height = window.innerHeight;
 
-var dots = [];
-for (var i = 0; i < 1000; i++)
-    dots.push(new GenerateDot());
-
-function draw() {
+function set_dimensions() {
 	canvas.node.width  = window.innerWidth;
 	canvas.node.height = window.innerHeight;
+}
+set_dimensions();
 
+function create_dots() {
+	d = [];
+	for (var i = 0; i < 2000; i++)
+		d.push(new GenerateDot());
+	return d;
+}
+dots = create_dots();
+
+function draw() {
     canvas.context.fillStyle = "black";
+	canvas.context.clearRect(0, 0, canvas.node.width, canvas.node.height);
 	canvas.context.fillRect(0, 0, canvas.node.width, canvas.node.height);
   
     for (var i = 0; i < dots.length; i++) {
@@ -40,16 +47,24 @@ function draw() {
         }
     }
 }
+setInterval(draw,33);
 
+// -- event listeners --
 canvas.node.addEventListener('mousemove', function(event) {
 	mousePos = new MousePosition(event);
-}, false);
+});
+
+window.addEventListener('resize', function(event) {
+	set_dimensions();
+	dots = create_dots();
+});
 
 // -- utility functions --
 function get_distance(point1,point2) {
 	return Math.sqrt(Math.pow(point1.x-point2.x,2)+Math.pow(point1.y-point2.y,2));
 }
 
+/*
 function find_nearest_dot(dot) {
 	var nearest;
     for (var i = 0; i < dots.length; i++) {
@@ -63,13 +78,12 @@ function find_nearest_dot(dot) {
     }
     return nearest;
 }
+*/
 
 function find_nearest_dots(dot) {
-    nearest = dots.slice();
-	nearest.sort(function(a, b){
+	return dots.slice().sort(function(a, b){
         return get_distance(a,dot) - get_distance(b,dot);
-    });
-    return nearest.slice(0,5);
+    }).slice(0,5);
 }
 
 // -- objects --
@@ -83,5 +97,3 @@ function MousePosition(event) {
     this.x = event.clientX - rect.left;
 	this.y = event.clientY - rect.top;
 }
-
-setInterval(draw,33);
